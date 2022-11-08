955d84d08d2e58842890e43aeed240dfccc57381
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-11-08 18:42:50 -0300

    Add method to getGameInfo

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -7,6 +7,20 @@
 
 const axios = require("axios");
 
+async function getGameInfo(slug) {
+  const jsdom = require("jsdom");
+  const { JSDOM } = jsdom;
+  const body = await axios.get(`https://www.gog.com/game/${slug}`);
+  const dom = new JSDOM(body.data);
+
+  const description = dom.window.document.querySelector(".description");
+  return {
+    rating: "FREE",
+    short_description: description.textContent.slice(0, 160),
+    description: description.innerHTML,
+  };
+}
+
 module.exports = {
   populate: async (param) => {
     const gogApiUrl = `https://www.gog.com/games/ajax/filtered?mediaType=game&page=1&sort=popularity`;
@@ -15,6 +29,6 @@ module.exports = {
       data: { products },
     } = await axios.get(gogApiUrl);
 
-    console.log(products);
+    console.log(await getGameInfo(products[0].slug));
   },
 };
