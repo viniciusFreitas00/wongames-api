85becbc6863a11db2c0ff70694c73fce7167e411
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-11-08 12:50:39 -0300

    Change LeftMenuFooter

---
+++ admin/src/components/LeftMenu/LeftMenuFooter/index.js
@@ -0,0 +1,32 @@
+/**
+ *
+ * LeftMenuFooter
+ *
+ */
+
+import React from "react";
+import Wrapper, { A } from "./Wrapper";
+
+function LeftMenuFooter() {
+  // PROJECT_TYPE is an env variable defined in the webpack config
+  // eslint-disable-next-line no-undef
+  const projectType = PROJECT_TYPE;
+
+  return (
+    <Wrapper>
+      <div className="poweredBy">
+        poweredBy &nbsp;
+        <A
+          key="website"
+          href="https://reactavancado.com.br"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Avançado
+        </A>
+      </div>
+    </Wrapper>
+  );
+}
+
+export default LeftMenuFooter;
